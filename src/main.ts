4436e14c5b5f999494c525fa99896b39e4bf08f5
Ahmad D. Y. Nugroho <ahmaddwi700@gmail.com>
2022-05-28 16:01:16 +0700

    feat: chiper & dechiper

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,25 @@
-import { hello } from './caesar'
-
-hello()
+import { chiper, dechiper } from './caesar'
+;[
+    {
+        msg: 'osaka, shizuku, kasumin, asaka, karin, mia chi, kawaii.',
+        key: '1221122',
+    },
+    {
+        msg: 'tokimeki ga doko?! vivid world',
+        key: '111121',
+    },
+    {
+        msg: 'ayumu lanzhu zong shioriko nijigasaki saiko.',
+        key: '212',
+    },
+].forEach((el) => {
+    const encryptedMsg = chiper(el.msg, el.key)
+    const decryptedMsg = dechiper(encryptedMsg, el.key)
+    console.log({
+        msg: el.msg,
+        key: el.key,
+        encryptedMsg,
+        decryptedMsg,
+        status: el.msg === decryptedMsg ? 'success' : 'failed',
+    })
+})
---
+++ src/utils.ts
@@ -0,0 +1,31 @@
+const shuffleText = (text: string): string => {
+    return text
+        .split('')
+        .map((s) => ({ s, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map((s) => s.s)
+        .join('')
+}
+export const pair = (mapKey: string, mapText: string): string[] => {
+    let map = []
+
+    for (let i = 0; i < mapKey.length; i++) {
+        for (let j = 0; j < mapText.length; j++) {
+            map.push(mapKey[i] + mapText[j])
+        }
+    }
+
+    return map
+        .map((el) => ({ el, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map((el) => el.el)
+}
+
+// console.log(' abcdefghijklmnopqrstuvwxyz'.length)
+// console.log('hblgmxjconyrf,q?!e.pkv azdiutsw')
+// console.log(shuffleText('hblgmxjconyrf,q?!e.pkv azdiutsw'))
+// console.log('abcdefghijklmopqrstuvwxyzabcdefghijklmopqrstuvwxyz')
+// const x = shuffleText('abcdefghijklmopqrstuvwxyzabcdefghijklmopqrstuvwxyz')
+// const y = splitTwo(x)
+// console.log(x)
+// console.log(y)
